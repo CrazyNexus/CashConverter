94333b20e5f80fe7e597263e798dff9c886476db
Thomas Dubiel <td@crazynexus.de>
2016-12-20 23:40:42 +0100

    finished

---
+++ CashConverter/CurrencyConverter.cs
@@ -0,0 +1,43 @@
+﻿//
+// CurrencyConverter.cs
+//
+// Created by Thomas Dubiel on 19.12.2016
+// Copyright 2016 Thomas Dubiel. All rights reserved.
+//
+using System;
+namespace CashConverter
+{
+	public class CurrencyConverter
+	{
+		string input;
+
+		public CurrencyConverter(string input)
+		{
+			this.input = input;
+		}
+
+		public string ConvertCurrency()
+		{
+			var output = "";
+
+			if (input.Length < 1)
+			{
+				output = "Please enter something";
+			}
+			else {
+				try
+				{
+					var result = Convert.ToDouble(input) * 0.69;
+					output = "$" + input + " = £" + result;
+				}
+				catch (Exception e)
+				{
+					output = "Please enter something";
+					Console.WriteLine("Error in convertion: " + e.Message);
+				}
+			}
+
+			return output;
+		}
+	}
+}
---
+++ iOS/ViewController.cs
@@ -0,0 +1,40 @@
+﻿//
+// ViewController.cs
+//
+// Created by Thomas Dubiel on 18.12.2016
+// Copyright 2016 Thomas Dubiel. All rights reserved.
+//
+using System;
+
+using UIKit;
+
+namespace CashConverter.iOS
+{
+	public partial class ViewController : UIViewController
+	{
+
+		public ViewController(IntPtr handle) : base(handle)
+		{
+		}
+
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+
+			// Perform any additional setup after loading the view, typically from a nib.
+			Button.AccessibilityIdentifier = "myButton";
+			Button.TouchUpInside += delegate
+			{
+				var convert = new CurrencyConverter(amountTextField.Text);
+				var result = convert.ConvertCurrency();
+				resultLabel.Text = result;
+			};
+		}
+
+		public override void DidReceiveMemoryWarning()
+		{
+			base.DidReceiveMemoryWarning();
+			// Release any cached data, images, etc that aren't in use.		
+		}
+	}
+}
